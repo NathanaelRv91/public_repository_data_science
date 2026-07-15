12fe49513c3f41bae069aab096519d9dd552c070
Nathaneal Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-07-15 14:30:50 -0600

    Update player_model_view.sql

--- snowflake_dbt_models/models/intermediate/player_model_view.sql
+++ snowflake_dbt_models/models/intermediate/player_model_view.sql
@@ -1,38 +1,78 @@
 
--- merge our all time stats into our career profiles --
-create or replace view PRNG_STAGE.PUBLIC.PRODUCTCATEGORYDATA(
-	MARKETNAME,
-	RETAILERNAME,
-	BANNERNAME,
-	CHANNELNAME,
-	FORMATNAME,
-	YEAR,
-	PRODUCTCATEGORY,
-	SPLIT
-) as
-SELECT
-        r.marketname,
-        r.retailername,
-        r.bannername,
-        r.channelname,
-        r.formatname,
-        r.year,
-        IFNULL(b.productcategory, IFNULL(y.productcategory, IFNULL(m.productcategory, d.productcategory))) productcategory,
-        IFNULL(b.percentage_split, IFNULL(y.percentage_split, IFNULL(m.percentage_split, d.percentage_split)))/100 split
 
-      FROM
-        PRNG.PUBLIC.PRNGRETAILERDATA r
-      LEFT OUTER JOIN PRNG.PUBLIC.prngproductcategory_banner b on
-        b.retailer = r.retailername and b.banner = r.bannername and b.format =  r.formatname and b.channel = r.channelname and b.market = r.marketname and b.year = YEAR(r.YEAR)
-      LEFT OUTER JOIN PRNG.PUBLIC.prngproductcategory_year y on
-        y.format =  r.formatname and y.channel = r.channelname and y.market = r.marketname and y.year = YEAR(r.YEAR) and b.productcategory is null
-      LEFT OUTER JOIN PRNG.PUBLIC.prngproductcategory_market m on
-        m.format =  r.formatname and m.channel = r.channelname and m.market = r.marketname and b.productcategory is null and y.productcategory is null
-      LEFT OUTER JOIN PRNG.PUBLIC.prngproductcategory_defaults d on
-        d.format =  r.formatname and d.channel = r.channelname and b.productcategory is null and y.productcategory is null and m.productcategory is null
--- WE use the same model view from POWER_BI.PRODUCTCATEGORYDATA -- 
-          WHERE r.MARKETNAME = 'United States'
-        AND DATE_PART(YEAR,r.YEAR) BETWEEN 2024 AND 2031
-        AND r.FORMATNAME IN ('Pureplay-1P','Pureplay-3P','Drive','Social Commerce','Omnichannel-1P','Omnichannel-3P')
-        AND r.CHANNELNAME IN ('Mass Merchandise','Department Stores','Beauty Specialists','Pharma & Health','Cash & Carry/Club','Hyper-Stores','Supermarkets & Neighbourhood Stores')      
-;
+-- CREATE Model for AI Tools -- 
+
+WITH career_stats AS (
+SELECT PLAYER_ID,
+PLAYER_TEAM_ID,
+YEAR_INT, 
+SUM(MIN_PLAYED) AS min_played,
+SUM(POINTS) AS points, 
+SUM(assists) AS assists, 
+SUM(blocks) AS blocks,
+SUM(steals) AS steals, 
+SUM(FGA) AS fg_attempts, 
+DIV0(SUM(FGM),SUM(FGA)) AS fg_pct, 
+DIV0(SUM(PT3_FGM),SUM(PT3_ATT)) AS pct_3pt, 
+SUM(DRB) AS drb,
+SUM(ORB) AS orb, 
+SUM(TRB) AS trb, 
+COUNT(DISTINCT GAME_ID) AS games_played,
+SUM(PF) AS personal_fouls, 
+SUM(TOS) AS turnovers, 
+LISTAGG(comment) AS all_comments_season
+FROM {{ ref('ALL_TIME_PLAYER_STATISTICS')}} 
+GROUP BY 1,2,3 ORDER BY 1,2,3
+),
+
+team_stats AS (
+    SELECT TEAMID, 
+    TEAMNAME,
+    DATE_PART(YEAR,GAMEDATE) AS YEAR_TM, 
+    COUNT(DISTINCT GAMEID) AS team_games_played,
+    SUM(REBOUNDSDEFENSIVE) AS team_drb, 
+    SUM(REBOUNDSOFFENSIVE) AS team_orb,
+    SUM(REBOUNDSTOTAL) AS team_trb,
+    SUM(STEALS)AS team_steals, 
+    SUM(BLOCKS) AS team_blocks, 
+    SUM(TURNOVERS) AS team_turnovers, 
+    SUM(Q1POINTS + Q2POINTS + Q3POINTS + Q4POINTS) AS team_points,
+    SUM(THREEPOINTERSATTEMPTED) AS team_3pt_fga,
+    SUM(THREEPOINTERSMADE) AS team_3pt_fgm,
+    DIV0(SUM(THREEPOINTERSMADE),SUM(THREEPOINTERSATTEMPTED)) AS team_pct_3pt,
+    SUM(FREETHROWSATTEMPTED) AS team_fta, 
+    SUM(FREETHROWSMADE) AS team_ftm, 
+    DIV0(SUM(FREETHROWSMADE),SUM(FREETHROWSATTEMPTED)) AS team_pct_ft,
+    SUM(WIN) AS team_season_wins,
+    SUM(FIELDGOALSATTEMPTED) AS team_fga, 
+    SUM(FIELDGOALSMADE) AS team_fgm, 
+    DIV0(SUM(FIELDGOALSMADE),SUM(FIELDGOALSATTEMPTED)) AS team_pct_fg
+    FROM {{ref('ALL_TIME_TEAM_STATISTICS')}} 
+GROUP BY 1,2,3
+)
+
+SELECT a.*,
+    b.TEAMNAME, 
+    b.team_drb, 
+    b.team_orb, 
+    b.team_trb, 
+    b.team_steals, 
+    b.team_blocks, 
+    b.team_turnovers, 
+    b.team_points, 
+    b.team_3pt_fga, 
+    b.team_3pt_fgm, 
+    b.team_pct_3pt, 
+    b.team_fga, 
+    b.team_fgm, 
+    b.team_pct_fg,
+    b.team_fta, 
+    b.team_ftm, 
+    b.team_pct_ft, 
+    b.team_season_wins, 
+    DIV0(b.team_season_wins,team_games_played) AS win_pct, 
+    FROM career_stats a JOIN team_stats b 
+    ON a.YEAR_INT = b.YEAR_TM AND a.PLAYER_TEAM_ID = b.TEAMID
+	--WE ONLY Want post-merger data for our training model for forecasting -- 
+    WHERE a.YEAR_INT >= 1976;
+
