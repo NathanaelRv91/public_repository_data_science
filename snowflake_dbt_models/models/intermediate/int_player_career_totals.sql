071f89d2e411b6583fdaecb077cac780289b04ff
Nathaneal Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-07-16 07:48:26 -0600

    Update int_player_career_totals.sql

--- snowflake_dbt_models/models/intermediate/int_player_career_totals.sql
+++ snowflake_dbt_models/models/intermediate/int_player_career_totals.sql
@@ -1,4 +1,42 @@
-{{ config(materialized = 'table') }}
+{{ config(materialized = 'table',
+  tags = 'nba_career_stats')}}
+
+WITH career_stats as (
+  SELECT player_id, 
+    COUNT(DISTINCT player_team_id) AS teams_played_for, 
+    COUNT(DISTINCT YEAR_INT) AS seasons_played,
+    SUM(games_played) AS games_played,
+  -- total stats --
+    SUM(games_played) AS games_played,
+    SUM(min_played) AS min_played, 
+    SUM(points) AS career_points, 
+    SUM(trb) AS career_trb, 
+    SUM(drb) AS career_drb, 
+    SUM(orb) AS career_orb, 
+    SUM(assists) AS career_assists, 
+    SUM(blocks) AS career_blocks,
+    SUM(turnovers) AS career_turnovers, 
+    SUM(personal_fouls) AS personal_fouls,
+  -- career shootint -- 
+    SUM(fg_attemps) as career_fga, 
+    SUM(fg_made) AS career_fgm, 
+    DIV0(SUM(fg_made),SUM(fg_attempts)) AS career_fg_pct, 
+    
+    
+  
+  
+
+
+  )
+
+SELECT a.*. 
+    b.
+
+  FROM career_stats a 
+    JOIN {{ref('player_details_source')}} b
+      ON a.player_id = b.personid
+
+
 
 
 
