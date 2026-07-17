dfca4d7838c91edac989fcca595add13d9b9aab6
Nathaneal Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-07-17 07:49:40 -0600

    Update season_stats_conversion.sql

--- snowflake_dbt_models/macros/season_stats_conversion.sql
+++ snowflake_dbt_models/macros/season_stats_conversion.sql
@@ -1,7 +1,7 @@
 {%- macro season_totals_to_per_season(stat_col, game_count, season, decimal_places = 3)-%}
-{% set seasons = [2016,2017,2018,2019,2020,2021,2022,2023,2024,2025] %}
+{% set seasons = [2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026] %}
   SELECT 
 {% for {{season}} in seasons %}
-    {{ column_name }} / {{game_count}} AS {{season}}_{{stat_col}}
+    {{ stat_col }} / {{game_count}} AS {{season}}_{{stat_col}}
   {% end for %}
 {%- endmacro -%}
