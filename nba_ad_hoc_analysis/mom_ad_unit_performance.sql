018240a6d263686701600f64d53f4d05468f9fd4
Nathaneal Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-07-20 18:06:59 -0600

    Update mom_ad_unit_performance.sql

--- nba_ad_hoc_analysis/mom_ad_unit_performance.sql
+++ nba_ad_hoc_analysis/mom_ad_unit_performance.sql
@@ -83,6 +83,5 @@ lag(sum(conversions),31,0) OVER(Partition By geo_company_id,suffixless_company_n
   END AS
   last_month_impressions
 FROM {{ ref('fct_player_master_stats') }}
-WHERE date >='2024-01-01'
-group by 1,2,3,4,5,6,7,8,9,10,11
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
 order by 1,2,3,4,5,6,7,8,9,10,11
