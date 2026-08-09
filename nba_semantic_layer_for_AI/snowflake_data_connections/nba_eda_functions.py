75251660dff68c61cbb78190b657becdeca2aa8b
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-08-09 14:26:11 -0600

    Update nba_eda_functions.py

--- nba_semantic_layer_for_AI/snowflake_data_connections/nba_eda_functions.py
+++ nba_semantic_layer_for_AI/snowflake_data_connections/nba_eda_functions.py
@@ -1,6 +1,9 @@
 
 
 
+
+
+
 import pandas as pd
 import numpy as np
 import datetime
@@ -8,8 +11,8 @@ import snowflake.connector
 
 def pull_player_list():
     conn = snowflake.connector.connect(
-    user= '*********6858841',
-    password='************2027!',
+    user= 'JANDERSON6858841',
+    password='********2027!',
     account='MNZAVFE-MM97348',
     warehouse='COMPUTE_WH',
     database='NBA_DB',
@@ -65,3 +68,32 @@ def pull_player_stats():
     #df_stats.columns = ['INDEX','FIRST_NAME','LAST_NAME','PLAYER_ID','GAME_ID','GAME_DATE','PLAYER_TEAM_CITY','PLAYER_TEAM_NAME','OPP_TEAM_CITY','OPP_TEAM_NAME','SEASON_TYPE','GM_LABEL','GM_SUBLABEL','GM_NUMBER','WIN','HOME','MIN_PLAYED','POINTS','ASSISTS','BLOCKS','STEALS','FGA','FGM','FG_PCT','PT3_ATT','PT3_FGM','PT3_PCT','FTA','FTM','FT_PCT','DRB','ORB','TRB','PF','TOS','PLUS_MINUS','PLAYER_TEAM_ID','OPP_TEAM_ID','COMMENT','POS','GAME_TIMESTAMP','YEAR_INT']
     return df_stats
 
+
+def pull_player_view():
+    conn = snowflake.connector.connect(
+    user='JANDERSON6858841',
+    password='********2027!',
+    account='MNZAVFE-MM97348',
+    warehouse='COMPUTE_WH',
+    database='NBA_DB',
+    schema='REPORTS',
+    role = 'ACCOUNTADMIN')
+
+    cursor = conn.cursor()
+    sql_stats = """
+        SELECT * FROM NBA_DB.REPORTS.PLAYER_STATS_VIEW
+                WHERE YEAR_SEASON >= 2021
+            """
+    try:
+        cursor.execute(sql_stats)
+        one_row_stats = cursor.fetch_pandas_all()
+        print(f"Successfully loaded data!:",one_row_stats.head(5))
+    finally:
+        cursor.close()
+        conn.close()
+
+    df_player_view = pd.DataFrame(one_row_stats)
+    df_player_view.to_csv('load_player_statistics_fcast_view.csv')
+    ## PLAYER STATS COLUMNS ##
+    #df_stats.columns = ['INDEX','FIRST_NAME','LAST_NAME','PLAYER_ID','GAME_ID','GAME_DATE','PLAYER_TEAM_CITY','PLAYER_TEAM_NAME','OPP_TEAM_CITY','OPP_TEAM_NAME','SEASON_TYPE','GM_LABEL','GM_SUBLABEL','GM_NUMBER','WIN','HOME','MIN_PLAYED','POINTS','ASSISTS','BLOCKS','STEALS','FGA','FGM','FG_PCT','PT3_ATT','PT3_FGM','PT3_PCT','FTA','FTM','FT_PCT','DRB','ORB','TRB','PF','TOS','PLUS_MINUS','PLAYER_TEAM_ID','OPP_TEAM_ID','COMMENT','POS','GAME_TIMESTAMP','YEAR_INT']
+    return df_player_view
