692082a03fb2da251d2f6d6c63af4d8731ec5e3c
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-08-10 13:54:41 -0600

    Create logistic_regression_classifier_for_retired_players_inference.py

---
+++ nba_semantic_layer_for_AI/nba_models_for_inference/player_forecast_modelling/logistic_regression_classifier_for_retired_players_inference.py
@@ -0,0 +1,22 @@
+from sklearn.model_selection import train_test_split
+from sklearn import datasets, linear_model, metrics
+import pandas as pd
+import numpy as np
+
+## Predict if a player will retire --
+nba_data = pd.read_csv('nba_10_year_all_star_stats_report.csv')
+nba_data['NUMBER_TM'] = nba_data['NUMBER_TM'].fillna("NONE")
+
+X = nba_data[['FROMYEAR','FGM','FT_PCT','POINTS','FTM','TOS','DRB','ORB']]
+y = nba_data['IS_ACTIVE']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+pd.DataFrame(X_train).to_csv('test_x_data.csv')
+reg = linear_model.LogisticRegression(max_iter=10000, random_state=42)
+reg.fit(X_train, y_train)
+
+y_pred = reg.predict(X_test)
+
+print(f"Logistic Regression model accuracy: {metrics.accuracy_score(y_test, y_pred) * 100:.2f}%")
+y_pred = pd.DataFrame(y_pred)
+y_pred.to_csv('test_fcast_model_test.csv')
