d41f02f1ad7ef14c751026b5f9139b61cfc840f1
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-08-13 13:31:18 -0600

    Update nba_eda_functions.py

--- nba_semantic_layer_for_AI/snowflake_data_connections/nba_eda_functions.py
+++ nba_semantic_layer_for_AI/snowflake_data_connections/nba_eda_functions.py
@@ -95,3 +95,75 @@ def pull_player_view():
     ## PLAYER STATS COLUMNS ##
     #df_stats.columns = ['INDEX','FIRST_NAME','LAST_NAME','PLAYER_ID','GAME_ID','GAME_DATE','PLAYER_TEAM_CITY','PLAYER_TEAM_NAME','OPP_TEAM_CITY','OPP_TEAM_NAME','SEASON_TYPE','GM_LABEL','GM_SUBLABEL','GM_NUMBER','WIN','HOME','MIN_PLAYED','POINTS','ASSISTS','BLOCKS','STEALS','FGA','FGM','FG_PCT','PT3_ATT','PT3_FGM','PT3_PCT','FTA','FTM','FT_PCT','DRB','ORB','TRB','PF','TOS','PLUS_MINUS','PLAYER_TEAM_ID','OPP_TEAM_ID','COMMENT','POS','GAME_TIMESTAMP','YEAR_INT']
     return df_player_view
+
+def pull_team_view():
+    conn = snowflake.connector.connect(
+    user='JANDERSON6858841',
+    password='********2027!',
+    account='MNZAVFE-MM97348',
+    warehouse='COMPUTE_WH',
+    database='NBA_DB',
+    schema='REPORTS',
+    role = 'ACCOUNTADMIN')
+
+    cursor = conn.cursor()
+
+    sql_teams = """CREATE OR REPLACE TABLE team_stats_temp AS (
+ SELECT TEAMID,
+    TEAMNAME,
+    CASE WHEN EXTRACT (MONTH FROM GAMEDATE) <= 7 THEN DATE_PART(YEAR, GAMEDATE) - 1 ELSE DATE_PART(YEAR,GAMEDATE) END AS YEAR_TM,
+    COUNT(DISTINCT GAMEID) AS team_games_played,
+    SUM(REBOUNDSDEFENSIVE) AS team_drb,
+    SUM(REBOUNDSOFFENSIVE) AS team_orb,
+    SUM(REBOUNDSTOTAL) AS team_trb,
+    SUM(STEALS)AS team_steals,
+    SUM(ASSISTS) AS team_assists,
+    SUM(BLOCKS) AS team_blocks,
+    SUM(TURNOVERS) AS team_turnovers,
+    SUM(Q1POINTS + Q2POINTS + Q3POINTS + Q4POINTS) AS team_points,
+    SUM(THREEPOINTERSATTEMPTED) AS team_3pt_fga,
+    SUM(THREEPOINTERSMADE) AS team_3pt_fgm,
+    DIV0(SUM(THREEPOINTERSMADE),SUM(THREEPOINTERSATTEMPTED)) AS team_pct_3pt,
+    SUM(FREETHROWSATTEMPTED) AS team_fta,
+    SUM(FREETHROWSMADE) AS team_ftm,
+    DIV0(SUM(FREETHROWSMADE),SUM(FREETHROWSATTEMPTED)) AS team_pct_ft,
+    SUM(WIN) AS team_season_wins,
+    SUM(FIELDGOALSATTEMPTED) AS team_fga,
+    SUM(FIELDGOALSMADE) AS team_fgm,
+    DIV0(SUM(FIELDGOALSMADE),SUM(FIELDGOALSATTEMPTED)) AS team_pct_fg
+    FROM ALL_TIME_TEAM_STATISTICS
+GROUP BY 1,2,3);
+
+
+CREATE OR REPLACE VIEW team_stats_view AS (
+    SELECT
+       a.*,
+       b.abbreviation,
+       b.city,
+       b.state,
+       b.year_founded,
+       c.arena,
+       c.arenacapacity,
+       c.owner,
+       c.headcoach,
+       c.generalmanager,
+       c.instagram           AS instagram_link,
+       c.facebook            AS fb_link,
+       c.twitter             AS x_link
+FROM team_stats_temp a
+            JOIN TEAM_DATA.TEAM_LIST b
+              ON a.teamid = b.team_id
+            LEFT JOIN TEAM_DATA.TEAM_DETAILS c
+               ON b.team_id = c.team_id
+                );"""
+    try:
+        cursor.execute(sql_teams)
+        one_row_stats = cursor.fetch_pandas_all()
+        print(f"Successfully loaded data!:",one_row_stats.head(5))
+    finally:
+        cursor.close()
+        conn.close()
+
+    df_team_view = pd.DataFrame(one_row_stats)
+    df_team_view.to_csv('load_team_statistics_fcast_view.csv')
+    return df_team_view
