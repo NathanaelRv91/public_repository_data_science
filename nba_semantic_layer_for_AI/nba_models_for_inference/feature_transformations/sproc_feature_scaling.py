1cccf6aa3ddf93d308d575c7dfdf154cdeb93328
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-08-19 11:23:04 -0600

    Create sproc_feature_scaling.py

---
+++ nba_semantic_layer_for_AI/nba_models_for_inference/feature_transformations/sproc_feature_scaling.py
@@ -0,0 +1,7 @@
+from imbalance.over_sampling import SMOTE
+from snowflake.snowpark.session import Session
+import snowflake.snowpark.functions as F
+import snowflake.snowpark.types as T
+from snowflake.ml.modeling.preprocessing import *
+import pandas as pd 
+
