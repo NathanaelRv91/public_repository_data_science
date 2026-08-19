a63866167daf8b769f525651e1b967f8594da409
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-08-19 12:35:17 -0600

    Update register_sproc_feature_scaling.py

--- nba_semantic_layer_for_AI/nba_models_for_inference/feature_transformations/register_sproc_feature_scaling.py
+++ nba_semantic_layer_for_AI/nba_models_for_inference/feature_transformations/register_sproc_feature_scaling.py
@@ -2,7 +2,7 @@ sproc_oversample_smote = session.sproc.register(func=sproc_oversample_smote,
                                                 name='sproc_oversample_smote', 
                                                 is_permanent=True, 
                                                 replace=True,
-                                                stage_location='@ML_NBA',
+                                                stage_location='@NBA_STAGE',
                                                 packages=[f'snowflake-snowpark-context',
                                                           f'skicit-learn==1.5.0',
                                                           f'pandas==2.2.3])
