2c6993aa5cc4a9027510c796a0601f88e66ec0fc
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-08-19 11:24:32 -0600

    Update sproc_feature_scaling.py

--- nba_semantic_layer_for_AI/nba_models_for_inference/feature_transformations/sproc_feature_scaling.py
+++ nba_semantic_layer_for_AI/nba_models_for_inference/feature_transformations/sproc_feature_scaling.py
@@ -1,7 +1,10 @@
 from imbalance.over_sampling import SMOTE
-from snowflake.snowpark.session import Session
 import snowflake.snowpark.functions as F
 import snowflake.snowpark.types as T
 from snowflake.ml.modeling.preprocessing import *
 import pandas as pd 
+from snowflake.connector.pandas_tools import write_pandas
+import snowflake.connector as sconn
 
+from snowflake.snowpark.context import get_active_session
+session = get_active_session()
