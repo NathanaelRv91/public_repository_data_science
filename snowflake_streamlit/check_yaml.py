5698008670f189e6504be4b0424df44315b50b4e
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-09-02 09:20:22 -0600

    Update check_yaml.py

--- snowflake_streamlit/check_yaml.py
+++ snowflake_streamlit/check_yaml.py
@@ -1,7 +1,7 @@
 ## CHECK my Semantic Layer Config before Uploading to @NBA_DB.REPORTS.NBA_STAGE/nba_semantic_models.yml
 import yaml
 
-with open("nba_stat_models_streamlit.yml", "r") as f:
+with open("nba_semantic_models.yml", "r") as f:
     yaml.safe_load(f)
 
 print("YAML is valid")
