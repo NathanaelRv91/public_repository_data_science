c5cc34f57362080757f2a04be152a61a880287e2
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-09-07 10:09:20 -0600

    Update rest_api_review_stages_for_etl_pipelinse.py

--- rest_api_workflow/rest_api_review_stages_for_etl_pipelinse.py
+++ rest_api_workflow/rest_api_review_stages_for_etl_pipelinse.py
@@ -1,10 +1,11 @@
 import requests
+import pandas as pd
 
 url = "https://ucxmqbt-aj89853.snowflakecomputing.com/api/v2/statements"
 
 headers = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer eyJU********cAyR64SmSDpg",
+    "Authorization": "Bearer eyJraWQi*********fOD9Ei47WhygjeKaP8hcAyR64SmSDpg",
     "Accept": "application/json",
     "User-Agent": "myApplicationName/1.0",
 }
@@ -18,9 +19,14 @@ payload = {
 response = requests.post(url, json=payload, headers=headers)
 print(response.headers)
 data_response = response.json()
+list_stage = []
 for key, value in data_response.items():
     if key == 'data':
         print(f"FOUND DATA!: {value}")
+        for j in value:
+            list_stage.append(j)
     else:
         pass
 print(data_response)
+
+pd.DataFrame(list_stage).to_csv('write_pandas_stage_summary.csv')
