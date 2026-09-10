5f5290b863cb81dbf9524ef2d542592445ed7cf6
Nathanael Richardson <134963471+NathanaelRv91@users.noreply.github.com>
2026-09-10 11:25:29 -0600

    Add SQL query for player points forecasting model

---
+++ nba_semantic_layer_for_AI/nba_models_for_inference/player_forecast_modelling/call_player_points_model_from_local_table.sql
@@ -0,0 +1,8 @@
+
+
+SELECT player_id, 
+full_name AS PLAYER_NAME, 
+season, 
+NBA_DB.REPORTS.player_forecasting!predict(player_id, season, steals, assists, rebounds, blocks) AS player_forecast 
+FROM NBA_DB.REPORTS.int_lakers_team_data
+
